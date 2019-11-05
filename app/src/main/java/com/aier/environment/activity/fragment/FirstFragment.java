e69b61a9c5018b5bad95f0b211c67cf5d8b4164e
admin <380129462@qq.com>
2019-11-05 14:35:36 +0800

    提交

--- app/src/main/java/com/aier/environment/activity/fragment/FirstFragment.java
+++ app/src/main/java/com/aier/environment/activity/fragment/FirstFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import com.aier.environment.JGApplication;
@@ -38,8 +39,11 @@ import okhttp3.Response;
 public class FirstFragment extends BaseFragment {
     private Context mContext;
     public TextView tv_city,tv_date,tv_wendu,tv_feng,tv_weather,
-            tv_date_1,tv_today_1,tv_date_2,tv_today_2,tv_date_3,tv_today_3,tv_date_4,tv_today_4,tv_date_5,tv_today_5;
+            tv_date_1,tv_today_1,tv_date_2,tv_today_2,tv_date_3,tv_today_3,tv_date_4,tv_today_4,
+            tv_date_5,tv_today_5,tv_wendu_1,tv_wendu_2,tv_wendu_3,tv_wendu_4,tv_wendu_5,tv_weather_1,
+            tv_weather_2,tv_weather_3,tv_weather_4,tv_weather_5;
 
+    private ImageView iv_weather_1,iv_weather_2,iv_weather_3,iv_weather_4,iv_weather_5,iv_air;
     private LocationService locationService;
     private BDLocation mLocation;
     public String city;
@@ -48,9 +52,6 @@ public class FirstFragment extends BaseFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = this.getActivity();
-        locationService = JGApplication.locationService;
-        locationService.registerListener(mListener);//是否应该在onStart中注册
-        locationService.start();
     }
 
 
@@ -72,6 +73,22 @@ public class FirstFragment extends BaseFragment {
         tv_today_4 = view.findViewById(R.id.tv_today_4);
         tv_date_5= view.findViewById(R.id.tv_date_5);
         tv_today_5 = view.findViewById(R.id.tv_today_5);
+        tv_wendu_1 = view.findViewById(R.id.tv_wendu_1);
+        tv_wendu_2 = view.findViewById(R.id.tv_wendu_2);
+        tv_wendu_3 = view.findViewById(R.id.tv_wendu_3);
+        tv_wendu_4 = view.findViewById(R.id.tv_wendu_4);
+        tv_wendu_5 = view.findViewById(R.id.tv_wendu_5);
+        tv_weather_1 = view.findViewById(R.id.tv_weather_1);
+        tv_weather_2 = view.findViewById(R.id.tv_weather_2);
+        tv_weather_3 = view.findViewById(R.id.tv_weather_3);
+        tv_weather_4 = view.findViewById(R.id.tv_weather_4);
+        tv_weather_5 = view.findViewById(R.id.tv_weather_5);
+        iv_weather_1 = view.findViewById(R.id.iv_weather_1);
+        iv_weather_2 = view.findViewById(R.id.iv_weather_2);
+        iv_weather_3 = view.findViewById(R.id.iv_weather_3);
+        iv_weather_4 = view.findViewById(R.id.iv_weather_4);
+        iv_weather_5 = view.findViewById(R.id.iv_weather_5);
+        iv_air = view.findViewById(R.id.iv_air);
         return view;
     }
 
@@ -154,27 +171,53 @@ public class FirstFragment extends BaseFragment {
                             WeatherBean.ResultBean.ForecastBean forecastBean;
                             SimpleDateFormat format1 = new SimpleDateFormat("MM/dd");
                             String data;
+                            String type;
+                            String wendu;
                             for(int i=0;i<forecastBeans.size();i++){
                                 forecastBean = forecastBeans.get(i);
                                 data = forecastBean.getDate();
+                                type=forecastBean.getType();
+                                wendu = forecastBean.getLow().substring(2,forecastBean.getLow().length())
+                                        +"~"+forecastBean.getHigh().substring(2,forecastBean.getHigh().length());
                                 if(i==0){
-                                    tv_feng.setText(forecastBean.getFengxiang()+forecastBean.getFengli());
+                                    String fengli = forecastBean.getFengli();
+                                    fengli =  fengli.substring(9,fengli.length());
+                                    int index = fengli.indexOf("级");
+                                    fengli = fengli.substring(0,index+1);
+                                    tv_feng.setText(forecastBean.getFengxiang()+fengli);
                                     tv_weather.setText(forecastBean.getType());
-                                    tv_date_1.setText(format1.format(new Date(System.currentTimeMillis())));
+                                    tv_date_1.setText(data.substring(0,2));
                                     tv_today_1.setText("今天");
+
+                                    tv_wendu_1.setText(wendu);
+                                    setWeatherPic(type,iv_air);
+                                    setWeatherPic(type,iv_weather_1);
+                                    tv_weather_1.setText(forecastBean.getType());
                                 }else if(i==1){
                                     tv_date_2.setText(data.substring(0,2));
                                     tv_today_2.setText(data.substring(2,data.length()));
+                                    tv_wendu_2.setText(wendu);
+                                    setWeatherPic(type,iv_weather_2);
+                                    tv_weather_2.setText(forecastBean.getType());
                                 }else if(i==2){
                                     tv_date_3.setText(data.substring(0,2));
                                     tv_today_3.setText(data.substring(2,data.length()));
+                                    tv_wendu_3.setText(wendu);
+                                    setWeatherPic(type,iv_weather_3);
+                                    tv_weather_3.setText(forecastBean.getType());
                                 }else if(i==3){
                                     tv_date_4.setText(data.substring(0,2));
                                     tv_today_4.setText(data.substring(2,data.length()));
+                                    tv_wendu_4.setText(wendu);
+                                    setWeatherPic(type,iv_weather_4);
+                                    tv_weather_4.setText(forecastBean.getType());
                                 }else if(i==4){
                                     tv_date_5.setText(data.substring(0,2));
                                     data = forecastBean.getDate();
                                     tv_today_5.setText(data.substring(2,data.length()));
+                                    tv_wendu_5.setText(wendu);
+                                    setWeatherPic(type,iv_weather_5);
+                                    tv_weather_5.setText(forecastBean.getType());
                                 }
                             }
                         }
@@ -185,16 +228,48 @@ public class FirstFragment extends BaseFragment {
 
     }
 
+    private void setWeatherPic(String type,ImageView imageView) {
+        Log.i("sss","type   "+type);
+        if(type.equals("晴")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_qing));
+        }else if(type.equals("阴")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_yin));
+        }else if(type.equals("小雨")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_xiaoyu));
+        }else if(type.equals("中雨")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_zhongyu));
+        }else if(type.equals("大雨")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_dayu));
+        }else if(type.equals("小雪")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_xiaoxue));
+        }else if(type.equals("大雪")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_daxue));
+        }else if(type.equals("雷雨")){
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_leiyu));
+        }else {
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_duoyun));//多云
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
+        locationService = JGApplication.locationService;
+        locationService.registerListener(mListener);//是否应该在onStart中注册
+        locationService.start();
         isFirst = true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationService.unregisterListener(mListener); // 注销掉监听
+        locationService.stop();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        locationService.unregisterListener(mListener); // 注销掉监听
-        locationService.stop();
+
     }
 }
