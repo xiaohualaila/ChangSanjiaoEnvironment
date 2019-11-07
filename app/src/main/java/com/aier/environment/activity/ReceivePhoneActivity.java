d87a29a362c6c9fbc8d57afd7edd9e00fc2a7ed2
admin <380129462@qq.com>
2019-11-07 17:53:11 +0800

    提交

--- app/src/main/java/com/aier/environment/activity/ReceivePhoneActivity.java
+++ app/src/main/java/com/aier/environment/activity/ReceivePhoneActivity.java
@@ -8,18 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
 import com.aier.environment.R;
 import com.aier.environment.utils.SingleSocket;
 import com.aier.environment.utils.ToastUtil;
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 import cn.jpush.im.android.api.JMessageClient;
 import cn.jpush.im.android.api.model.Conversation;
 import cn.jpush.im.android.api.model.GroupInfo;
@@ -28,9 +31,9 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 
-public class ReceivePhoneActivity extends BaseActivity implements View.OnClickListener{
+public class ReceivePhoneActivity extends BaseActivity implements View.OnClickListener {
     private Context mContext;
-    private Button btn_receive, btn_cancel,btn_cancel_;
+    private Button btn_receive, btn_cancel, btn_cancel_;
     private LinearLayout ll_toOther_people;
     private boolean isCallToOther;
     private boolean mIsSingle;
@@ -39,18 +42,19 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
 
     private Socket mSocket;
     private String roomId;
-    private String userName,nickname;
+    private String userName, nickname;
     private GroupInfo mGroupInfo;
     private List<UserInfo> mMemberInfoList = new ArrayList<UserInfo>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.mContext = this;
         setContentView(R.layout.activity_receive_phone);
         Intent intent = getIntent();
-        isCallToOther = intent.getBooleanExtra("isCallToOther",false);
-        mIsSingle = intent.getBooleanExtra("mIsSingle",false);
-        mGroupId = intent.getLongExtra("mGroupId",0);
+        isCallToOther = intent.getBooleanExtra("isCallToOther", false);
+        mIsSingle = intent.getBooleanExtra("mIsSingle", false);
+        mGroupId = intent.getLongExtra("mGroupId", 0);
         mTargetId = intent.getStringExtra("mTargetId");//要呼叫人的id
         roomId = intent.getStringExtra("meet_id");
         ll_toOther_people = findViewById(R.id.ll_toOther_people);
@@ -61,28 +65,28 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
         btn_cancel.setOnClickListener(this);
         btn_cancel_.setOnClickListener(this);
         socket();
-        Log.i("aaa","onCreate+++++++++++");
+        Log.i("aaa", "onCreate+++++++++++");
     }
 
     private void socket() {
-        if(isCallToOther){//打给别人的情况
+        if (isCallToOther) {//打给别人的情况
             Random r = new Random();
             roomId = String.valueOf(r.nextInt(1000));
             btn_cancel_.setVisibility(View.VISIBLE);
-        }else {
+        } else {
             ll_toOther_people.setVisibility(View.VISIBLE);
         }
         UserInfo myInfo = JMessageClient.getMyInfo();
-        userName=myInfo.getUserName();
+        userName = myInfo.getUserName();
         nickname = myInfo.getNickname();
-        Log.i("ddd", "userName " +userName + "房间号随机数"+roomId);
-        String url = "https://www.airer.com?userid="+userName+"&type=mobile";
+        Log.i("ddd", "userName " + userName + "房间号随机数" + roomId);
+        String url = "https://www.airer.com?userid=" + userName + "&type=mobile";
         mSocket = SingleSocket.getInstance().getSocket(url);
         //注册  事件
 
         mSocket.on("message", messageData);
-        if(isCallToOther){
-            if(mIsSingle){//单人聊天
+        if (isCallToOther) {
+            if (mIsSingle) {//单人聊天
                 try {
                     JSONObject object = new JSONObject();
                     object.put("userid", mTargetId);//mTargetId 对方id
@@ -90,9 +94,9 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
                     object.put("data", "");
                     object.put("room", roomId);
                     object.put("type", "is-join");
-                    Log.i("ddd",object.toString());
+                    Log.i("ddd", object.toString());
                     mSocket.emit("call-user", object.toString());// 单个用户呼叫
-                    Log.i("ddd","单个用户呼叫 call-user 用户id "+mTargetId);
+                    Log.i("ddd", "单个用户呼叫 call-user 用户id " + mTargetId);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -103,17 +107,17 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
                     mGroupInfo = (GroupInfo) conv.getTargetInfo();
                     mMemberInfoList = mGroupInfo.getGroupMembers();
                     UserInfo userInfo;
-                    for(int i=0;i<mMemberInfoList.size();i++){
+                    for (int i = 0; i < mMemberInfoList.size(); i++) {
                         userInfo = mMemberInfoList.get(i);
-                        if(userInfo.getUserName()!=userName){
+                        if (userInfo.getUserName() != userName) {
                             JSONObject object = new JSONObject();
                             object.put("userid", userInfo.getUserName());//mTargetId 对方id
                             object.put("data", "");
                             object.put("room", roomId);
                             object.put("type", "is-join");
-                            Log.i("ddd",object.toString());
+                            Log.i("ddd", object.toString());
                             mSocket.emit("call-user", object.toString());// 单个用户呼叫
-                            Log.i("ddd","单个用户呼叫 call-user 用户id "+mTargetId);
+                            Log.i("ddd", "单个用户呼叫 call-user 用户id " + mTargetId);
                         }
                     }
                 } catch (JSONException e) {
@@ -131,21 +135,26 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
                 @Override
                 public void run() {
                     JSONObject data = (JSONObject) args[0];
-                    if(data.optString("type").equals("joined")){
-                      //  {"type":"joined","data":{"userid":"0002"}}
-                        Log.i("ddd","message "+data.toString());
-                        toMeetingActivity();
-                    }else if(data.optString("type").equals("join_reject")){
-                        ToastUtil.shortToast(ReceivePhoneActivity.this,"用户拒接视频通话");
+                    if (data.optString("type").equals("joined")) {
+                        //  {"type":"joined","data":{"userid":"0002"}}
+                        Log.i("ddd", "message " + data.toString());
+                        Intent intent = new Intent(ReceivePhoneActivity.this, MeetingActivity.class);
+                        intent.putExtra("meet_id", roomId);
+                        intent.putExtra("nickname", nickname);
+                        intent.putExtra("isCallToOther", isCallToOther);
+                        intent.putExtra("mIsSingle", mIsSingle);
+                        startActivity(intent);
                         finish();
-                        Log.i("ddd","message "+data.toString());
-                    } else if(data.optString("type").equals("position")){
+                    } else if (data.optString("type").equals("join_reject")) {
+                        ToastUtil.shortToast(ReceivePhoneActivity.this, "用户拒接视频通话");
+                        finish();
+                        Log.i("ddd", "message " + data.toString());
+                    } else if (data.optString("type").equals("position")) {
 
-                    }else if(data.optString("type").equals("online-user")){
+                    } else if (data.optString("type").equals("online-user")) {
 
-                    }
-                    else {
-                        Log.i("ddd","message "+data.toString());
+                    } else {
+                        Log.i("ddd", "message " + data.toString());
                         //finish();
                     }
                 }
@@ -155,35 +164,26 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_receive:
-                String[]  perm = {Permission.RECORD_AUDIO,Permission.CAMERA,Permission.READ_EXTERNAL_STORAGE,Permission.WRITE_EXTERNAL_STORAGE};
-
-                if (AndPermission.hasPermissions(ReceivePhoneActivity.this,perm)){
-                    toMeetingActivity();
-                }else {
-                    AndPermission.with(ReceivePhoneActivity.this).runtime().permission(Permission.RECORD_AUDIO,Permission.CAMERA,Permission.READ_EXTERNAL_STORAGE,Permission.WRITE_EXTERNAL_STORAGE).onDenied(new Action<List<String>>() {
-                        @Override
-                        public void onAction(List<String> data) {
-                            Toast.makeText(ReceivePhoneActivity.this, "请打开音视频,文件读写权限", Toast.LENGTH_SHORT).show();
-                        }
-                    }).onGranted(new Action<List<String>>() {
-                        @Override
-                        public void onAction(List<String> data) {
-                            try {
-                                JSONObject object = new JSONObject();
-                                object.put("userid", userName);
-                                object.put("room", roomId);
-                                object.put("from_type", "mobile");
-                                mSocket.emit("join", object.toString());// 接听电话
-                                Log.i("ddd","接听电话 发送 "+ object.toString());
-                                toMeetingActivity();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                }
+                toMeetingActivity();
+//                String[] perm = {Permission.RECORD_AUDIO, Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE};
+//
+//                if (AndPermission.hasPermissions(ReceivePhoneActivity.this, perm)) {
+//                        toMeetingActivity();
+//                } else {
+//                    AndPermission.with(ReceivePhoneActivity.this).runtime().permission(Permission.RECORD_AUDIO, Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE).onDenied(new Action<List<String>>() {
+//                        @Override
+//                        public void onAction(List<String> data) {
+//                            Toast.makeText(ReceivePhoneActivity.this, "请打开音视频,文件读写权限", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }).onGranted(new Action<List<String>>() {
+//                        @Override
+//                        public void onAction(List<String> data) {
+//                            toMeetingActivity();
+//                        }
+//                    }).start();
+//                }
                 break;
             case R.id.btn_cancel:
             case R.id.btn_cancel_:
@@ -193,7 +193,7 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
                     object.put("room", "");
                     object.put("from_type", "mobile");
                     mSocket.emit("join", object.toString());// 拒绝电话
-                    Log.i("ddd","拒绝电话 发送 "+ object.toString());
+                    Log.i("ddd", "拒绝电话 发送 " + object.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -203,13 +203,17 @@ public class ReceivePhoneActivity extends BaseActivity implements View.OnClickLi
     }
 
     private void toMeetingActivity() {
-        Intent intent = new Intent(ReceivePhoneActivity.this, MeetingActivity.class);
-        intent.putExtra("meet_id", roomId);
-        intent.putExtra("nickname", nickname);
-        intent.putExtra("isCallToOther", isCallToOther);
-        intent.putExtra("mIsSingle", mIsSingle);
-        startActivity(intent);
-        finish();
+        try {
+            JSONObject object = new JSONObject();
+            object.put("userid", userName);
+            object.put("room", roomId);
+            object.put("from_type", "mobile");
+            mSocket.emit("join", object.toString());// 接听电话
+            Log.i("ddd", "接听电话 发送 " + object.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- app/src/main/java/com/aier/environment/activity/fragment/FirstFragment.java
+++ app/src/main/java/com/aier/environment/activity/fragment/FirstFragment.java
@@ -9,20 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+
 import com.aier.environment.JGApplication;
 import com.aier.environment.R;
 import com.aier.environment.location.service.LocationService;
 import com.aier.environment.model.WeatherBean;
+import com.aier.environment.utils.ToastUtil;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.google.gson.Gson;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -38,16 +44,17 @@ import okhttp3.Response;
 
 public class FirstFragment extends BaseFragment {
     private Context mContext;
-    public TextView tv_city,tv_date,tv_wendu,tv_feng,tv_weather,
-            tv_date_1,tv_today_1,tv_date_2,tv_today_2,tv_date_3,tv_today_3,tv_date_4,tv_today_4,
-            tv_date_5,tv_today_5,tv_wendu_1,tv_wendu_2,tv_wendu_3,tv_wendu_4,tv_wendu_5,tv_weather_1,
-            tv_weather_2,tv_weather_3,tv_weather_4,tv_weather_5;
+    public TextView tv_city, tv_date, tv_wendu, tv_feng, tv_weather,
+            tv_date_1, tv_today_1, tv_date_2, tv_today_2, tv_date_3, tv_today_3, tv_date_4, tv_today_4,
+            tv_date_5, tv_today_5, tv_wendu_1, tv_wendu_2, tv_wendu_3, tv_wendu_4, tv_wendu_5, tv_weather_1,
+            tv_weather_2, tv_weather_3, tv_weather_4, tv_weather_5;
 
-    private ImageView iv_weather_1,iv_weather_2,iv_weather_3,iv_weather_4,iv_weather_5,iv_air;
+    private ImageView iv_weather_1, iv_weather_2, iv_weather_3, iv_weather_4, iv_weather_5, iv_air;
     private LocationService locationService;
     private BDLocation mLocation;
     public String city;
     private boolean isFirst = true;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,21 +64,21 @@ public class FirstFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_first,container,false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
         tv_city = view.findViewById(R.id.tv_city);
         tv_date = view.findViewById(R.id.tv_date);
         tv_wendu = view.findViewById(R.id.tv_wendu);
         tv_feng = view.findViewById(R.id.tv_feng);
-        tv_weather= view.findViewById(R.id.tv_weather);
-        tv_date_1= view.findViewById(R.id.tv_date_1);
+        tv_weather = view.findViewById(R.id.tv_weather);
+        tv_date_1 = view.findViewById(R.id.tv_date_1);
         tv_today_1 = view.findViewById(R.id.tv_today_1);
-        tv_date_2= view.findViewById(R.id.tv_date_2);
+        tv_date_2 = view.findViewById(R.id.tv_date_2);
         tv_today_2 = view.findViewById(R.id.tv_today_2);
-        tv_date_3= view.findViewById(R.id.tv_date_3);
+        tv_date_3 = view.findViewById(R.id.tv_date_3);
         tv_today_3 = view.findViewById(R.id.tv_today_3);
-        tv_date_4= view.findViewById(R.id.tv_date_4);
+        tv_date_4 = view.findViewById(R.id.tv_date_4);
         tv_today_4 = view.findViewById(R.id.tv_today_4);
-        tv_date_5= view.findViewById(R.id.tv_date_5);
+        tv_date_5 = view.findViewById(R.id.tv_date_5);
         tv_today_5 = view.findViewById(R.id.tv_today_5);
         tv_wendu_1 = view.findViewById(R.id.tv_wendu_1);
         tv_wendu_2 = view.findViewById(R.id.tv_wendu_2);
@@ -101,9 +108,9 @@ public class FirstFragment extends BaseFragment {
         public void onReceiveLocation(BDLocation location) {
             // TODO Auto-generated method stub
             if (null != location && location.getLocType() != BDLocation.TypeServerError) {
-                mLocation= location;
+                mLocation = location;
                 city = location.getCity();
-                if(isFirst==true&&!TextUtils.isEmpty(city)){
+                if (isFirst == true && !TextUtils.isEmpty(city)) {
                     isFirst = false;
                     getWeatherData();
                 }
@@ -124,14 +131,14 @@ public class FirstFragment extends BaseFragment {
 
         try {
             object.put("method", "ENVIRONMENTAPI_GETCITYWEARTHER");
-            city = city.substring(0,city.length()-1);
-         //   Log.i("sss",city);
-            object.put("city",city );
+            city = city.substring(0, city.length() - 1);
+            //   Log.i("sss",city);
+            object.put("city", city);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-       // Log.i("sss",object.toString());
+        // Log.i("sss",object.toString());
 
         RequestBody body = RequestBody.create(json, object.toString());
         Request request = new Request.Builder()//创建Request 对象。
@@ -143,7 +150,13 @@ public class FirstFragment extends BaseFragment {
         call.enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, IOException e) {
-              //  Log.i("sss",e.getMessage());
+                //  Log.i("sss",e.getMessage());
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ToastUtil.shortToast(getActivity(),"网络异常！");
+                    }
+                });
             }
 
             @Override
@@ -157,66 +170,66 @@ public class FirstFragment extends BaseFragment {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                       // Log.i("sss",string);
+                        // Log.i("sss",string);
                         Gson gson = new Gson();
-                        WeatherBean weatherBean =  gson.fromJson(string, WeatherBean.class);
-                        WeatherBean.ResultBean resultBean =  weatherBean.getResult();
-                        if(weatherBean.isSuccess()){
-                            tv_city.setText(resultBean.getCity()+"市");
+                        WeatherBean weatherBean = gson.fromJson(string, WeatherBean.class);
+                        WeatherBean.ResultBean resultBean = weatherBean.getResult();
+                        if (weatherBean.isSuccess()) {
+                            tv_city.setText(resultBean.getCity() + "市");
                             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");// HH:mm:ss
                             Date date = new Date(System.currentTimeMillis());
                             tv_date.setText(simpleDateFormat.format(date));
-                            tv_wendu.setText(resultBean.getWendu()+"℃");
+                            tv_wendu.setText(resultBean.getWendu() + "℃");
                             List<WeatherBean.ResultBean.ForecastBean> forecastBeans = resultBean.getForecast();
                             WeatherBean.ResultBean.ForecastBean forecastBean;
                             SimpleDateFormat format1 = new SimpleDateFormat("MM/dd");
                             String data;
                             String type;
                             String wendu;
-                            for(int i=0;i<forecastBeans.size();i++){
+                            for (int i = 0; i < forecastBeans.size(); i++) {
                                 forecastBean = forecastBeans.get(i);
                                 data = forecastBean.getDate();
-                                type=forecastBean.getType();
-                                wendu = forecastBean.getLow().substring(2,forecastBean.getLow().length())
-                                        +"~"+forecastBean.getHigh().substring(2,forecastBean.getHigh().length());
-                                if(i==0){
+                                type = forecastBean.getType();
+                                wendu = forecastBean.getLow().substring(2, forecastBean.getLow().length())
+                                        + "~" + forecastBean.getHigh().substring(2, forecastBean.getHigh().length());
+                                if (i == 0) {
                                     String fengli = forecastBean.getFengli();
-                                    fengli =  fengli.substring(9,fengli.length());
+                                    fengli = fengli.substring(9, fengli.length());
                                     int index = fengli.indexOf("级");
-                                    fengli = fengli.substring(0,index+1);
-                                    tv_feng.setText(forecastBean.getFengxiang()+fengli);
+                                    fengli = fengli.substring(0, index + 1);
+                                    tv_feng.setText(forecastBean.getFengxiang() + fengli);
                                     tv_weather.setText(forecastBean.getType());
-                                    tv_date_1.setText(data.substring(0,2));
+                                    tv_date_1.setText(data.substring(0, 2));
                                     tv_today_1.setText("今天");
 
                                     tv_wendu_1.setText(wendu);
-                                    setWeatherPic(type,iv_air);
-                                    setWeatherPic(type,iv_weather_1);
+                                    setWeatherPic(type, iv_air);
+                                    setWeatherPic(type, iv_weather_1);
                                     tv_weather_1.setText(forecastBean.getType());
-                                }else if(i==1){
-                                    tv_date_2.setText(data.substring(0,2));
-                                    tv_today_2.setText(data.substring(2,data.length()));
+                                } else if (i == 1) {
+                                    tv_date_2.setText(data.substring(0, 2));
+                                    tv_today_2.setText(data.substring(2, data.length()));
                                     tv_wendu_2.setText(wendu);
-                                    setWeatherPic(type,iv_weather_2);
+                                    setWeatherPic(type, iv_weather_2);
                                     tv_weather_2.setText(forecastBean.getType());
-                                }else if(i==2){
-                                    tv_date_3.setText(data.substring(0,2));
-                                    tv_today_3.setText(data.substring(2,data.length()));
+                                } else if (i == 2) {
+                                    tv_date_3.setText(data.substring(0, 2));
+                                    tv_today_3.setText(data.substring(2, data.length()));
                                     tv_wendu_3.setText(wendu);
-                                    setWeatherPic(type,iv_weather_3);
+                                    setWeatherPic(type, iv_weather_3);
                                     tv_weather_3.setText(forecastBean.getType());
-                                }else if(i==3){
-                                    tv_date_4.setText(data.substring(0,2));
-                                    tv_today_4.setText(data.substring(2,data.length()));
+                                } else if (i == 3) {
+                                    tv_date_4.setText(data.substring(0, 2));
+                                    tv_today_4.setText(data.substring(2, data.length()));
                                     tv_wendu_4.setText(wendu);
-                                    setWeatherPic(type,iv_weather_4);
+                                    setWeatherPic(type, iv_weather_4);
                                     tv_weather_4.setText(forecastBean.getType());
-                                }else if(i==4){
-                                    tv_date_5.setText(data.substring(0,2));
+                                } else if (i == 4) {
+                                    tv_date_5.setText(data.substring(0, 2));
                                     data = forecastBean.getDate();
-                                    tv_today_5.setText(data.substring(2,data.length()));
+                                    tv_today_5.setText(data.substring(2, data.length()));
                                     tv_wendu_5.setText(wendu);
-                                    setWeatherPic(type,iv_weather_5);
+                                    setWeatherPic(type, iv_weather_5);
                                     tv_weather_5.setText(forecastBean.getType());
                                 }
                             }
@@ -228,25 +241,25 @@ public class FirstFragment extends BaseFragment {
 
     }
 
-    private void setWeatherPic(String type,ImageView imageView) {
-       // Log.i("sss","type   "+type);
-        if(type.equals("晴")){
+    private void setWeatherPic(String type, ImageView imageView) {
+        // Log.i("sss","type   "+type);
+        if (type.equals("晴")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_qing));
-        }else if(type.equals("阴")){
+        } else if (type.equals("阴")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_yin));
-        }else if(type.equals("小雨")){
+        } else if (type.equals("小雨")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_xiaoyu));
-        }else if(type.equals("中雨")){
+        } else if (type.equals("中雨")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_zhongyu));
-        }else if(type.equals("大雨")){
+        } else if (type.equals("大雨")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_dayu));
-        }else if(type.equals("小雪")){
+        } else if (type.equals("小雪")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_xiaoxue));
-        }else if(type.equals("大雪")){
+        } else if (type.equals("大雪")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_daxue));
-        }else if(type.equals("雷雨")){
+        } else if (type.equals("雷雨")) {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_leiyu));
-        }else {
+        } else {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather_duoyun));//多云
         }
     }
