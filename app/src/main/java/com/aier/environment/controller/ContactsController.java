87147c32924160930a2fa17178cf77a240a93066
admin <380129462@qq.com>
2019-11-07 15:52:42 +0800

    提交

--- app/src/main/java/com/aier/environment/controller/ContactsController.java
+++ app/src/main/java/com/aier/environment/controller/ContactsController.java
@@ -6,10 +6,8 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
-
 import com.activeandroid.ActiveAndroid;
 import com.aier.environment.JGApplication;
 import com.aier.environment.R;
@@ -24,20 +22,13 @@ import com.aier.environment.utils.pinyin.PinyinComparator;
 import com.aier.environment.utils.sidebar.SideBar;
 import com.aier.environment.view.ContactsView;
 import com.google.gson.Gson;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-
-import cn.jpush.im.android.api.ContactManager;
 import cn.jpush.im.android.api.JMessageClient;
-import cn.jpush.im.android.api.callback.GetUserInfoListCallback;
-import cn.jpush.im.android.api.model.UserInfo;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
