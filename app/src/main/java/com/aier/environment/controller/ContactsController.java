f808af71395ec6f3e47b8e32f0198d1f4cc4e537
admin <380129462@qq.com>
2019-11-07 15:52:10 +0800

    提交

--- app/src/main/java/com/aier/environment/controller/ContactsController.java
+++ app/src/main/java/com/aier/environment/controller/ContactsController.java
@@ -34,8 +34,6 @@ import java.util.Collections;
 import java.util.List;
 
 
-
-
 import cn.jpush.im.android.api.ContactManager;
 import cn.jpush.im.android.api.JMessageClient;
 import cn.jpush.im.android.api.callback.GetUserInfoListCallback;
@@ -60,7 +58,7 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
     private StickyListAdapter mAdapter;
     private TextView mAllContactNumber;
     private List<FriendEntry> forDelete = new ArrayList<>();
-
+    List<FriendBean.ResultBean.UsersBean> mListUsersBean =new ArrayList<>();
     private String appkey;
 
     public ContactsController(ContactsView mContactsView, FragmentActivity context) {
@@ -75,18 +73,18 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
         Intent intent = new Intent();
         switch (v.getId()) {
             case R.id.ib_goToAddFriend://标题栏加号添加好友
-               // intent.setClass(mContext, SearchForAddFriendActivity.class);
-              //  mContext.startActivity(intent);
+                // intent.setClass(mContext, SearchForAddFriendActivity.class);
+                //  mContext.startActivity(intent);
                 break;
             case R.id.verify_ll://验证消息
-                Log.i("sss","验证消息+++");
+                Log.i("sss", "验证消息+++");
 //                intent.setClass(mContext, VerificationMessageActivity.class);
 //                mContext.startActivity(intent);
 //                mContactsView.dismissNewFriends();
 //                mAllContactNumber.setVisibility(View.GONE);
                 break;
             case R.id.group_ll://群组
-                Log.i("sss","群组+++");
+                Log.i("sss", "群组+++");
                 intent.setClass(mContext, GroupActivity.class);
                 mContext.startActivity(intent);
                 break;
@@ -176,23 +174,131 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
 //        });
         // TODO: 2019/10/29 替换网络请求
 
+        OkHttpClient client2 = new OkHttpClient();//创建OkHttpClient对象。
+        MediaType json2 = MediaType.parse("application/json; charset=utf-8");
+        JSONObject object2 = new JSONObject();
+        JSONObject obj12 = new JSONObject();
+
+        try {
+            object2.put("method", "ENVIRONMENTAPI_IMUSERSLIST");
+            obj12.put("start", 0);
+            obj12.put("count", 100);
+            object2.put("params", obj12);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        //  Log.i("sss",object.toString());
+
+        RequestBody body2 = RequestBody.create(json2, object2.toString());
+        Request request2 = new Request.Builder()//创建Request 对象。
+                .url("http://121.41.52.56:3001/environmentalapi")
+                .post(body2)
+                .build();
+        Call call2 = client2.newCall(request2);
+        //请求加入调度
+        call2.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {//回调的方法执行在子线程。
+                    String str = null;
+                    try {
+                        str = response.body().string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Gson gson = new Gson();
+                    FriendBean bean = gson.fromJson(str, FriendBean.class);
+                    Log.i("sss", str);
+                    if (bean.isSuccess()) {
+                        List<FriendBean.ResultBean.UsersBean> list = bean.getResult().getUsers();
+                        mListUsersBean.clear();
+                        mListUsersBean.addAll(list);
+//                        if (list != null && list.size() != 0) {
+//                            ActiveAndroid.beginTransaction();
+//                            try {
+//                                for (int i = 0; i < list.size(); i++) {
+//                                    FriendBean.ResultBean.UsersBean usersBean = list.get(i);
+//                                    String displayName = usersBean.getUsername();
+//                                    String letter;
+//                                    if (!TextUtils.isEmpty(displayName.trim())) {
+//                                        ArrayList<HanziToPinyin.Token> tokens = HanziToPinyin.getInstance().get(displayName);
+//                                        StringBuilder sb = new StringBuilder();
+//                                        if (tokens != null && tokens.size() > 0) {
+//                                            for (HanziToPinyin.Token token : tokens) {
+//                                                if (token.type == HanziToPinyin.Token.PINYIN) {
+//                                                    sb.append(token.target);
+//                                                } else {
+//                                                    sb.append(token.source);
+//                                                }
+//                                            }
+//                                        }
+//                                        String sortString = sb.toString().substring(0, 1).toUpperCase();
+//                                        if (sortString.matches("[A-Z]")) {
+//                                            letter = sortString.toUpperCase();
+//                                        } else {
+//                                            letter = "#";
+//                                        }
+//                                    } else {
+//                                        letter = "#";
+//                                    }
+//                                    //避免重复请求时导致数据重复A
+//                                    FriendEntry friend = FriendEntry.getFriend(user, usersBean.getUsername(), appkey);
+//                                    if (null == friend) {
+//                                        friend = new FriendEntry(10201101L, usersBean.getUsername(), usersBean.getNickname(),
+//                                                usersBean.getNickname(), appkey, null, displayName, letter, user);
+//                                        friend.save();
+//                                        mList.add(friend);
+//                                    }
+//                                    forDelete.add(friend);
+//                                }
+//                                ActiveAndroid.setTransactionSuccessful();
+//                            } finally {
+//                                ActiveAndroid.endTransaction();
+//                            }
+//
+//
+//                        }
+//                        //其他端删除好友后,登陆时把数据库中的也删掉
+//                        List<FriendEntry> friends = JGApplication.getUserEntry().getFriends();
+//                        friends.removeAll(forDelete);
+//                        for (FriendEntry del : friends) {
+//                            del.delete();
+//                            mList.remove(del);
+//                        }
+//                        Collections.sort(mList, new PinyinComparator());//按拼音排序
+//                        mAdapter = new StickyListAdapter(mContext, mList, false);
+//                        mContactsView.setAdapter(mAdapter);
+                    }
+                }
+            }
+        });
+
+
+        ///////////////////////////////////
 
         OkHttpClient client = new OkHttpClient();//创建OkHttpClient对象。
         MediaType json = MediaType.parse("application/json; charset=utf-8");
         JSONObject object = new JSONObject();
         JSONObject obj1 = new JSONObject();
 
-            try {
-                object.put("method", "ENVIRONMENTAPI_IMADMINLIST");
-                obj1.put("start", 0);
-                obj1.put("count", 100);
-                object.put("params",obj1);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        try {
+            object.put("method", "ENVIRONMENTAPI_IMADMINLIST");
+            obj1.put("start", 0);
+            obj1.put("count", 100);
+            object.put("params", obj1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
-      //  Log.i("sss",object.toString());
+        //  Log.i("sss",object.toString());
 
         RequestBody body = RequestBody.create(json, object.toString());
         Request request = new Request.Builder()//创建Request 对象。
@@ -222,9 +328,10 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
                             }
                             Gson gson = new Gson();
                             FriendBean bean = gson.fromJson(str, FriendBean.class);
-                               Log.i("zzz", str);
+                      //      Log.i("zzz", str);
                             if (bean.isSuccess()) {
                                 List<FriendBean.ResultBean.UsersBean> list = bean.getResult().getUsers();
+                                list.addAll(mListUsersBean);
                                 if (list != null && list.size() != 0) {
                                     mContactsView.dismissLine();
                                     ActiveAndroid.beginTransaction();
@@ -282,7 +389,7 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
                                     }
 
 
-                                }else {
+                                } else {
                                     mContactsView.showLine();
                                 }
                                 //其他端删除好友后,登陆时把数据库中的也删掉
@@ -303,7 +410,6 @@ public class ContactsController implements View.OnClickListener, SideBar.OnTouch
             }
         });
     }
-
 
 
     @Override
